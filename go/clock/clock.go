aef4743d476441ca8faff46f50ff8f19b5a93239
Robbie Smith <robert.smith160@icloud.com>
2017-01-19 12:47:44 -0700

    Finishes go leap

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -4,22 +4,41 @@
 // You can document general stuff about the package here if you like.
 package clock
 
-// The value of testVersion here must match `targetTestVersion` in the file
-// clock_test.go.
+// import (
+// 	"fmt"
+// )
+
 const testVersion = 4
+
+// The value of testVersion here must match `targetTestVersion` in the file
+
+// clock_test.go.
 
 // Clock API as stub definitions.  No, it doesn't compile yet.
 // More details and hints are in clock_test.go.
 
-type Clock // Complete the type definition.  Pick a suitable data type.
+type Clock struct {
+	min int
+}
+
+// Complete the type definition.  Pick a suitable data type.
 
 func New(hour, minute int) Clock {
+	c := Clock{(60*hour + minute) % 1440}
+	_ = "breakpoint"
+	if c.min < 0 {
+		c.min += 1440
+	}
+	return c
 }
 
 func (Clock) String() string {
+	p := "1"
+	return p
 }
 
 func (Clock) Add(minutes int) Clock {
+	return Clock{100}
 }
 
 // Remember to delete all of the stub comments.
--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,8 +1,9 @@
+// This package will determine valid leap years.
 package leap
 
 const testVersion = 2
 
-// It's good style to write a comment here documenting IsLeapYear.
+// This function will determine valid leap years.
 func IsLeapYear(year int) bool {
 	if year%4 == 0 && year%100 != 0 {
 		return true
